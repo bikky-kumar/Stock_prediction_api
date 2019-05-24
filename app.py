d8fc02190ab2f494b2a3a9703efac21cf6522812
bikky <bikky.kumar.ie@gmail.com>
2019-05-24 19:47:04 +0100

    Linear regression working

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range=(0, 1))
 
+from sklearn.model_selection import train_test_split
+
+
 
 #How to start the app in virtualenv
 
@@ -56,9 +59,33 @@ def serve_predection(symbol):
     #Deleting values not required
     data = data.drop('timestamp', axis=1)
     data = data.drop('Date', axis=1)
+   
+   #seperating training and testing set
+    x = len(data)*0.8
+    #feature 
+    x = data.drop('Close', axis=1)
+    
+    #output
+    y = data['Close']
 
-    print(data)
-    return jsonify(dataForAllDays)   
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+    #implementing Linear regression 
+    from sklearn.linear_model import LinearRegression
+    model = LinearRegression()
+    model.fit(xTrain, yTrain)
+
+    #making prediction 
+    xTest['Predection'] = model.predict(xTest)
+    #rms = np.sqrt(np.mean(np.power((np.array(yTest)-np.array(xTest['Predection'])), 2)))
+    #print(rms)
+    print(yTest)
+    return jsonify(xTest.to_json(orient='split')) 
+
+   
+
+
+    #find rmse , then do the ltsm on jupyter notebook, 3. try to apply the ltsm here in python
 
 
 #How to load json data to a python df
