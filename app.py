2ab6f5bc439adbdb8dc22862bf95e9750b0b87df
bikky <bikky.kumar.ie@gmail.com>
2019-05-30 02:23:33 +0100

    final commit

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-from flask import Flask, jsonify, request 
-#pip3 install pandas
-#pip3 install sklearn
-import random, os, requests
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0, 1))
-
-from sklearn.model_selection import train_test_split
-
-
 
 #How to start the app in virtualenv
 
@@ -18,11 +6,40 @@ from sklearn.model_selection import train_test_split
 # then python app.py
 
 
-#Init app
-app = Flask(__name__)
+
+from flask import Flask, jsonify, request 
+#pip3 install pandas
+#pip3 install sklearn
+#pip install keras 
+#pip install tensorflow
+
+import random, os, sys, requests
+import pandas as pd
+import numpy as np
+import h5py
+
+#importing required libraries to perform LSTM 
+import tensorflow as tf
+graph = tf.get_default_graph()
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from keras.models import Model, model_from_json, load_model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+from sklearn.neural_network import MLPRegressor
+from keras.backend import clear_session
+#Disabling the AVX CPU warning
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
 
 #Correctly locate the database file
 basedir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.abspath('./models'))
+
+#Init app
+app = Flask(__name__)
 
 
 #calling alphavantage endpoint to retrive data for desired symbol
@@ -31,10 +48,11 @@ API_KEY = 'DKEHNKFEN5UFI1F7'
 def serve_predection(symbol):
     resp = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+symbol+'&outputsize=full&apikey='+API_KEY)
     if resp.status_code != 200:
-        # This means something went wrong.
         print(resp.status_code)
+
     result = resp.json()
     dataForAllDays = result['Time Series (Daily)']
+    
     #converting JSON result to dataframe  
     df = pd.DataFrame.from_dict(dataForAllDays)
     #transposing data
@@ -48,44 +66,57 @@ def serve_predection(symbol):
 
     data['timestamp'] = pd.to_datetime(data.Date, format='%Y-%m-%d')
 
-    #sorting the data
-    #data = df.sort_index(ascending=True, axis=0)
-
-    #separating dates, month and year
-    data['year'] = data['timestamp'].dt.year
-    data['month'] = data['timestamp'].dt.month
-    data['dayofweek'] = data['timestamp'].dt.dayofweek
-
-    #Deleting values not required
-    data = data.drop('timestamp', axis=1)
-    data = data.drop('Date', axis=1)
-   
-   #seperating training and testing set
-    x = len(data)*0.8
-    #feature 
-    x = data.drop('Close', axis=1)
+    #setting the index
+    data.index = data.timestamp
+    #sorting from dates
+    data = data.sort_index(ascending= True, axis = 0)
+    data.drop('timestamp', axis=1, inplace=True)
     
-    #output
-    y = data['Close']
+    
+    
+    #Here we only take the closing prices for last 60 days
+    dataset = data.iloc[-425:, 1:2].values
+    
+    #feature Scaling 
+    scaler = MinMaxScaler(feature_range = (0, 1))
+    dataset = dataset.reshape(-1,1)
+    dataset = dataset.astype(float)
+    dataset = scaler.fit_transform(dataset)
 
-    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size = 0.2, random_state = 0)
+    #reshapping the data 
+    data_array = []
+    for i in range(60, 425):
+        data_array.append(dataset[i-60:i, 0])
 
-    #implementing Linear regression 
-    from sklearn.linear_model import LinearRegression
-    model = LinearRegression()
-    model.fit(xTrain, yTrain)
+    data_array = np.array(data_array)
+    data_array = np.reshape(data_array, (data_array.shape[0], data_array.shape[1], 1))
 
-    #making prediction 
-    xTest['Predection'] = model.predict(xTest)
-    #rms = np.sqrt(np.mean(np.power((np.array(yTest)-np.array(xTest['Predection'])), 2)))
-    #print(rms)
-    print(yTest)
-    return jsonify(xTest.to_json(orient='split')) 
+
+    #Loading the Model
+    json_file = open('LSTM_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("LSTM_model.h5")
+    
+    #in our computation graph
+    #sending data_array to the model 
+    prediction = loaded_model.predict(data_array)
+    #performing tranform inverse on return data from the model
+    prediction = scaler.inverse_transform(prediction)
+    
+    #prepearing to send JSON response back
+    prediction = pd.DataFrame(data=prediction)
+    serve_pred = pd.DataFrame(index=range(0,len(prediction)),columns=['Close'])
+    serve_pred['Close'] = prediction[0]
+    
+    #This may a bug or an unsupported case in tensorflow keras backend: session is cached globally and is not cleared.
+    #So we have to clear it manually otherwise there can be only one prediction per session
+    clear_session()
+    return jsonify(serve_pred.to_json()) 
 
    
-
-
-    #find rmse , then do the ltsm on jupyter notebook, 3. try to apply the ltsm here in python
 
 
 #How to load json data to a python df
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
-#install venv for python if want to use on virtual server
+You need python 3 and above to run this application
+
+#to install all the dependencies 
+pip install -r requirements.txt
+
+once everything is installed 
+do the following: 
+
+make a new folder inside the folder where the downloaded file sits
+
+in the command prompt type the following:
+
+mkdir myproject
+cd myproject
+virtualenv venv
+
+in Linux or OS X
+venv/bin/activate
+
+in windows
+venv\scripts\activate
+
+#go to the flask_api directory 
+# type venv\Scripts\activate in terminal
+# then python app.py to start the app locally
